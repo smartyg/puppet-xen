deb5176bbc503b923707769e4e7836e8c74e4e57
Martijn Goedhart <goedhart.martijn@gmail.com>
2020-12-25 12:11:54 +0100

    update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,61 +1,54 @@
 # kate: replace-tabs on; indent-width 2; tab-width 2; indent-mode cstyle; word-wrap-column 140; word-wrap on;
 
 class xen (
-  Optional[String] $version = undef,
-  Boolean $install = true,
-  Boolean $managed = true,
-  Boolean $purge = true,
-  String $domain_config_dir = $::xen::paths::xen_domain_config_dir,
-  String $autostart_dir = $::xen::paths::xen_autostart_dir,
-  String $saved_domain_dir = $::xen::paths::saved_domain_dir,
-  String $default_vif_script = 'openvswitch',
+  Optional[String] $version    = undef,
+  Boolean $install             = true,
+  Boolean $managed             = true,
+  Boolean $purge               = true,
+  String $domain_config_dir    = $::xen::paths::xen_domain_config_dir,
+  String $autostart_dir        = $::xen::paths::xen_autostart_dir,
+  String $saved_domain_dir     = $::xen::paths::saved_domain_dir,
+  String $default_vif_script   = 'openvswitch',
   String $default_bridge_iface = 'eth0',
-  Boolean $restart_saved = true,
+  Boolean $restart_saved       = true,
 ) inherits xen::paths  {
 
   $packages = ['xen', 'xen-tools', 'xen-tools-xendomains-wait-disk']
   $services = ['xencommons.service', 'xendomains.service']
 
-  if($install)
+  if $install
   {
     each($packages) | $pkg | {
       unless defined(Package[$pkg]) {
         package{ $pkg:
-          before => Service['xendomains.service'],
+          before => Anchor['step-install-ready'],
           ensure => latest,
         }
       }
     }
   }
 
-  file { [$xendomain_systemd_dropin_dir, $xen_config_dir, $domain_config_dir, $autostart_dir, $saved_domain_dir]:
+  file { [$xendomain_systemd_dropin_dir, $xen_config_dir, $saved_domain_dir]:
+    before => Anchor['step-config-ready'],
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
 
-  file { $modprobe_loopback_file:
-    notify  => Service['xendomains.service'],
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/xen/xen-loopback.conf',
-  }
-
-  file { $modules_load_xen_modules_file:
-    notify  => Service['xendomains.service'],
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/xen/xen-modules.conf',
+  file { [$domain_config_dir, $autostart_dir]:
+    before => Anchor['step-config-ready'],
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    purge  => $purge,
   }
 
   if $restart_saved {
     file { $xendomain_systemd_dropin_file_save_dir:
       require => File[$xendomain_systemd_dropin_dir],
+      before  => Anchor['step-config-ready'],
       before  => Service['xendomains.service'],
       ensure  => file,
       owner   => 'root',
@@ -63,10 +56,16 @@ class xen (
       mode    => '0644',
       content => epp('xen/depend-on-save-dir.conf.epp', {'domain_save_path' => $saved_domain_dir}),
     }
+  } else {
+    file { $xendomain_systemd_dropin_file_save_dir:
+      before => Anchor['step-config-ready'],
+      esure  => absent,
+    }
   }
 
   file { $sysconfig_xencommons_file:
-    require => Package['xen'],
+    require => Anchor['step-install-ready'],
+    before  => Anchor['step-config-ready'],
     ensure  => file,
     owner   => 'root',
     group   => 'root',
@@ -75,7 +74,8 @@ class xen (
   }
 
   file { $sysconfig_xendomains_file:
-    require => Package['xen'],
+    require => Anchor['step-install-ready'],
+    before  => Anchor['step-config-ready'],
     ensure  => file,
     owner   => 'root',
     group   => 'root',
@@ -85,7 +85,8 @@ class xen (
   }
 
   file { $xl_config_file:
-    require => Package['xen'],
+    require => Anchor['step-install-ready'],
+    before  => Anchor['step-config-ready'],
     ensure  => file,
     owner   => 'root',
     group   => 'root',
@@ -94,20 +95,26 @@ class xen (
       'default_bridge_iface' => $default_bridge_iface}),
   }
 
-  if($managed)
+  if $managed
   {
     service { $services:
-      require => [
-        Package['xen'],
+      require => Anchor['step-config-ready'],
+      subscribe => [
         File[$sysconfig_xendomains_file],
         File[$sysconfig_xencommons_file],
-        File[$modules_load_xen_modules_file],
-        File[$modprobe_loopback_file],
         File[$xl_config_file],
-        File[$autostart_dir],
       ],
+      before => Anchor['step-start-ready'],
       ensure  => running,
       enable  => true,
     }
+  }
+
+  anchor {'step-install-ready': }
+  anchor {'step-config-ready':
+    require => Anchor['step-install'],
+  }
+  anchor {'step-start-ready':
+    require => Anchor['step-config'],
   }
 }
