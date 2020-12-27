b7004f3e7cd70c60b000de0010bb96b34e094e52
Martijn Goedhart <goedhart.martijn@gmail.com>
2020-12-27 15:40:50 +0100

    fix

--- lib/puppet/provider/domu/domu.rb
+++ lib/puppet/provider/domu/domu.rb
@@ -1,10 +1,11 @@
 # kate: replace-tabs on; indent-width 2; tab-width 2; indent-mode ruby;
 # kate: word-wrap-column 80; word-wrap on;
 
+require 'puppet/resource_api'
 require 'puppet/resource_api/simple_provider'
 
 # Implementation for the domu type using the Resource API.
-class Puppet::Provider::Domu::Domu
+class Puppet::Provider::Domu::Domu < Puppet::ResourceApi::SimpleProvider
   def get(context, names = nil)
     context.notice("get with #{names.inspect}")
     [
